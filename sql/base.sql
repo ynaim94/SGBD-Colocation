643d4622842414326a829a0cb7f9f0059522a6ba
marthaud <marthaud@053f6cbf-99b3-4a9d-881a-308454fd32ba>
2016-11-27 17:01:21 +0000

    bebug

--- sql/base.sql
+++ sql/base.sql
@@ -27,7 +27,7 @@ create table COLOCATION
     ID_COLOCATION                   NUMBER(3)              not null,
     ADRESSE_COLOCATION              CHAR(20)               not null,
     NOM_COLOCATION                  CHAR(20)               not null,
-    A_UNE_CAGNOTTE                  BOOLEAN                not null,
+    A_UNE_CAGNOTTE                  BYTE(1)                not null,
     ID_PERSONNE                     NUMBER(3)              not null,
     constraint pk_colocation primary key (ID_COLOCATION)
 );
@@ -96,7 +96,7 @@ create table CONTRAT_MEMBRE
     DATE_SORTIE                     DATE                   not null,	
     ID_COLOCATION                   NUMBER(3)              not null,
     ID_PERSONNE                     NUMBER(3)              not null,
-    constraint pk_versement primary key (ID_VERSEMENT)
+    constraint pk_versement primary key (ID_CONTRAT_MEMBRE)
 );
 
 
