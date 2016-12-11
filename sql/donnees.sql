77433bd91c37c9fc775bbf07292260a2ffd5d110
jvitrat <jvitrat@053f6cbf-99b3-4a9d-881a-308454fd32ba>
2016-12-11 18:47:05 +0000

    Fin d'ajout de donnees de test

--- sql/donnees.sql
+++ sql/donnees.sql
@@ -24,86 +24,63 @@ insert into PERSONNE values (  2  , 'Quiron'	 , 'Honore'   	, 'HonoreQuiron@gmai
 insert into PERSONNE values (  3  , 'Hachee'	 , 'Stephane' 	, 'StephaneHachee@gmail.com' 	) ;
 insert into PERSONNE values (  4  , 'Guibord' 	 , 'Bellamy'  	, 'BellamyGuibord@gmail.com' 	) ;
 insert into PERSONNE values (  5  , 'Bertrand' 	 , 'Marc'    	, 'MarcBertrand@gmail.com' 	) ;
+insert into PERSONNE values (  6  , 'Duval' 	 , 'Loic'    	, 'LoicDuval@gmail.com' 	) ;
+insert into PERSONNE values (  7  , 'Grandin' 	 , 'Eric'    	, 'EricGrandin@gmail.com' 	) ;
 
 commit ;
 
 
 -- COLOCATION (id_col, adr, nom, cagnotte, id_pers)
 
-insert into COLOCATION values (  1  , '63 rue des Coudriers'	  ,  'A'  ,  'Y' ,  1  ) ;
-insert into COLOCATION values (  2  , '15 Place du Jeu de Paume'  ,  'B'  ,  'Y' ,  1  ) ;
-insert into COLOCATION values (  3  , '29 rue des Lacs'   	  ,  'C'  ,  'Y' ,  1  ) ;
-insert into COLOCATION values (  4  , '36 rue des Soeurs'	  ,  'D'  ,  'Y' ,  3  ) ;
-insert into COLOCATION values (  5  , '37 rue Ernest Renan'	  ,  'E'  ,  'Y' ,  2  ) ;
-
+insert into COLOCATION values (  1  , '63 rue des Coudriers'	  ,  'A'  ,  'Y' ,  3  ) ;
+insert into COLOCATION values (  2  , '15 Place du Jeu de Paume'  ,  'B'  ,  'Y' ,  5  ) ;
 
 commit ;
 
 -- CONTRAT_MEMBRE (id_contrat, date entree, date sortie, id coloc, id pers)
 
 insert into CONTRAT_MEMBRE values (  1  ,  '01-JAN-2015'  , '01-JAN-2016' ,  1  ,  1  ) ;
-insert into CONTRAT_MEMBRE values (  2  ,  '27-JUN-2015'  , null	  ,  1  ,  4  ) ;
-insert into CONTRAT_MEMBRE values (  3  ,  '04-AUG-2015'  , null	  ,  2  ,  4  ) ;
-insert into CONTRAT_MEMBRE values (  4  ,  '30-AUG-2015'  , null	  ,  2  ,  4  ) ;
-insert into CONTRAT_MEMBRE values (  5  ,  '21-DEC-2015'  , null	  ,  2  ,  2  ) ;
+insert into CONTRAT_MEMBRE values (  2  ,  '27-JUN-2015'  , null	  ,  1  ,  2  ) ;
+insert into CONTRAT_MEMBRE values (  3  ,  '30-AUG-2015'  , null	  ,  1  ,  4  ) ;
+insert into CONTRAT_MEMBRE values (  4  ,  '21-DEC-2015'  , null	  ,  2  ,  6  ) ;
+insert into CONTRAT_MEMBRE values (  5  ,  '08-AUG-2015'  , null	  ,  2  ,  7  ) ;
 
 commit ;
 
--- ACHAT_COLOCATION (id, intitulé, date, montant, id_coloc, id_contrat_mb)
-
-insert into ACHAT_COLOCATION values ( 1 , 'ACHAT1' , '06-DEC-2016' , 1  ,  2) ;
-insert into ACHAT_COLOCATION values ( 2 , 'ACHAT2' , '03-JAN-2015' ,  2 ,  null) ;
-commit ;
-
--- BENEFICIAIRE (id_contrat_mb, id beneficiaire)
-
-insert into BENEFICIAIRE values (4, 1) ;
-insert into BENEFICIAIRE values (5, 1) ;
-
-commit ;
-
--- ACHAT_PERSONNEL (id, intitulé, date, montant, id_contrat_membre)
-
-insert into ACHAT_PERSONNEL values ( 1 , 'ACHAT PERSO 1' , '02-JAN-2016', 8000 , 3 ) ;
-
-commit ;
-/*
 -- VERSEMENT (id_vers, date, montant, id_contrat_mb_payeur, id_contrat_membre_receveur)
 
-insert into VERSEMENT values ( , , , , ) ;
-insert into VERSEMENT values ( , , , , ) ;
-insert into VERSEMENT values ( , , , , ) ;
-insert into VERSEMENT values ( , , , , ) ;
+insert into VERSEMENT values ( 1 , '10-NOV-2015' , 25 , 1 , 3 ) ;
 
 commit ;
 
 -- ABONDEMENT (id, date, montant, id_contrat_mb)
 
-insert into ABONDEMENT values ( , , , , ) ;
-insert into ABONDEMENT values ( , , , , ) ;
-insert into ABONDEMENT values ( , , , , ) ;
-insert into ABONDEMENT values ( , , , , ) ;
+insert into ABONDEMENT values ( 1 , '01-AUG-2015' , 50 , 2 ) ;
 
 commit ;
 
--- BENEFICIAIRE (id_contrat_mb, id beneficiaire)
 
-insert into BENEFICIAIRE values ( , , , , ) ;
-insert into BENEFICIAIRE values ( , , , , ) ;
-insert into BENEFICIAIRE values ( , , , , ) ;
-insert into BENEFICIAIRE values ( , , , , ) ;
+-- BENEFICIAIRE (id_contrat_mb, id achat_pers)
+
+insert into BENEFICIAIRE values ( 2 , 2 ) ;
+insert into BENEFICIAIRE values ( 3 , 2 ) ;
 
 commit ;
 
+
+--ACHAT_COLOCATION (id, intitulé, date, montant, id_coloc, id_contrat_mb)
+
+insert into ACHAT_COLOCATION values ( 1 , 'Television' , '10-JAN-2015' , 500 , 1 , 1 ) ;
+insert into ACHAT_COLOCATION values ( 2 , 'Frigo'      , '30-JUN-2015' , 399 , 1 , 2 ) ;
+insert into ACHAT_COLOCATION values ( 3 , 'Table'      , '10-JAN-2016' , 125 , 2 , 4 ) ;
+insert into ACHAT_COLOCATION values ( 4 , 'Canape'     , '10-SEP-2015' , 170 , 2 , 5 ) ;
+
+commit ;
 
 
 -- ACHAT_PERSONNEL (id, intitulé, date, montant, id_contrat_membre)
 
-insert into ACHAT_PERSONNEL values ( , , , , ) ;
-insert into ACHAT_PERSONNEL values ( , , , , ) ;
-insert into ACHAT_PERSONNEL values ( , , , , ) ;
-insert into ACHAT_PERSONNEL values ( , , , , ) ;
+insert into ACHAT_PERSONNEL values ( 1 , 'Lit' 	 , '30-JUN-2015' , 199 , 2 ) ;
+insert into ACHAT_PERSONNEL values ( 2 , 'Lampe' , '30-SEP-2015' , 29  , 1 ) ;
 
 commit ;
-
-*/
